5d7debf7cdc99cc472461afe5e263c8a7ce042a0
barerenate <renatw17@uia.no>
2022-11-02 12:28:05 +0100

    lagt inn razor pages for suggestions og fått alle funksjoner til å funke

---
+++ Pages/Suggestions/Details.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using rds_test.Data;
+using rds_test.Models;
+
+namespace rds_test.Pages.Suggestions
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly rds_test.Data.AppDbContext _context;
+
+        public DetailsModel(rds_test.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+      public Suggestion Suggestion { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.suggestion == null)
+            {
+                return NotFound();
+            }
+
+            var suggestion = await _context.suggestion.FirstOrDefaultAsync(m => m.case_num == id);
+            if (suggestion == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Suggestion = suggestion;
+            }
+            return Page();
+        }
+    }
+}
